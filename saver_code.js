1bbad0143b399cf5037e7f6a0e285d654aa6067c
hannahc258 <43076028+hannahc258@users.noreply.github.com>
2019-10-24 15:44:24 -0700

    Update saver_code.js

--- saver_code.js
+++ saver_code.js
@@ -1,2 +1,11 @@
+var options = {
+	files: [],
+	success: function () {
+		alert("Success! Files saved to your Dropbox.");
+	},
+	cancel: function () {},
+	error: function (errorMessage) {}
+};
+
 var button = Dropbox.createChooseButton(url, filename, options);
 document.getElementById("container").appendChild(button);
